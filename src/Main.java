379f649827cda7859fddb272000a475e17fbfe84
Александр <klerik.af.miass@gmail.com>
2025-01-02 14:53:48 +0500

    Home-work.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Проверка связи!");
+        System.out.println("Task 1");
+
+        String firstName = "Семён ";
+        String middleName = "Семёнович";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName + ".");
+
+        System.out.println("Task 2");
+
+        String fullNameCorrected = fullName.toUpperCase();
+        System.out.println("Ф.И.О. сотрудника — " + fullNameCorrected + ".");
+
+        System.out.println("Task 3");
+
+        String replacingLetter = fullName.replace("ё", "e");
+        System.out.println("Ф.И.О. сотрудника — " + replacingLetter + ".");
     }
 }
